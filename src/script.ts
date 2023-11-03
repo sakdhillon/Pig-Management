70156473364d53955825a9ac2d8dd1f0c786a045
Serena <serena14027@gmail.com>
2023-11-02 22:24:14 -0700

    made pig class

--- src/script.ts
+++ src/script.ts
@@ -4,19 +4,47 @@
 let message: string = ("hiiiiii peeepppssss");
 console.log(message);
 
+// PIG Categories
+
+enum PigType{
+    Grey = 'Grey',
+    Chestnut = 'Chestnut',
+    White = 'White',
+    Black = 'Black'
+}
+
+// Interfaces for dynamic field (based on the categoroes)
+interface DynamicFieldMap {
+    [PigType.Grey]: {
+        Swimming: number;
+    };
+    [PigType.Chestnut]: {
+        Language: string;
+    };
+    [PigType.White]: {
+        Running: number;
+    };
+    [PigType.Black]: {
+        Strength: number;
+    };
+}
 
 // PIG Class
-
 class Pig {
     name: string;
     height: number;
     weight: number;
+    category: keyof typeof PigType;
     personality: string;
 
-    constructor (n: string, h: number, w: number, p: string){
+    dynamicField: DynamicFieldMap[keyof typeof PigType];
+
+    constructor(n: string, h: number, w: number, c: keyof typeof PigType, p: string) {
         this.name = n;
         this.height = h;
         this.weight = w;
+        this.category = c;
         this.personality = p;
+        this.dynamicField = {} as DynamicFieldMap[keyof typeof PigType];
     }
 }
